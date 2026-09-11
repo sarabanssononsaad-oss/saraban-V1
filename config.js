55147d975e356545d1b0adcd357670db3fb8399d
sarabanssononsaad-oss <saraban.ssononsaad@gmail.com>
2026-09-11 16:29:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const APP_CONFIG={API_URL:"https://script.google.com/macros/s/AKfycbyeGgUxFexQ8nrQYF7xcf2haPOlt_yqiYxgKTdPCIAR5wxJib7llA1L4wKuuRR3K5oQ/exec",APP_NAME:"ระบบทะเบียนหนังสืออิเล็กทรอนิกส์",ORGANIZATION:"สำนักงานสาธารณสุขอำเภอโนนสะอาด",SESSION_KEY:"non_sa_at_registry_session_v2"};
+const APP_CONFIG={API_URL:"https://script.google.com/macros/s/AKfycbwpexB8ZQSsBHazYbhglFK6KBihJkza4h6EBNE28hKl3htMqcsi36CwXBC-EPPX09iQ/exec",APP_NAME:"ระบบทะเบียนหนังสืออิเล็กทรอนิกส์",ORGANIZATION:"สำนักงานสาธารณสุขอำเภอโนนสะอาด",SESSION_KEY:"non_sa_at_registry_session_v2"};
