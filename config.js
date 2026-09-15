e44c9659d20ff92da04218e84638640af2a2965c
sarabanssononsaad-oss <saraban.ssononsaad@gmail.com>
2026-09-15 15:52:05 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG={
   API_URL:'https://script.google.com/macros/s/AKfycbwpexB8ZQSsBHazYbhglFK6KBihJkza4h6EBNE28hKl3htMqcsi36CwXBC-EPPX09iQ/exec',
-  SESSION_KEY:'NONSAAT_V53_SESSION',
+  SESSION_KEY:'NONSAAT_V53_3_SESSION',
   ORGANIZATION:'สำนักงานสาธารณสุขอำเภอโนนสะอาด'
 };
